865e61fc4e7592bb9f95ab763213dc1cfda36670
DYLAN-PC\Dylan <lordofduct@lordofduct.com>
2018-04-06 16:24:59 -0400

    no message

--- Assets/Code/ScriptRunner.cs
+++ Assets/Code/ScriptRunner.cs
@@ -4,44 +4,34 @@ using UnityEngine;
 
 public class ScriptRunner : MonoBehaviour {
 
-    ScriptEngine _scriptEngine;
+    #region Fields
 
-	// Use this for initialization
-	void Start () {
-        _scriptEngine = new ScriptEngine(printMessage);
+    public TextAsset script;
+
+    private ScriptEngine _scriptEngine;
+
+    #endregion
+
+    #region CONSTRUCTOR
+
+    // Use this for initialization
+    void Start () {
+        _scriptEngine = this.gameObject.AddComponent<ScriptEngine>();
+        _scriptEngine.Init(Debug.Log);
     }
-    
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKey(KeyCode.Return))
+
+    #endregion
+
+    #region Methods
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Starting running script. Should Print 'TestPrint' every second, five times.");
-
-            string pythonSource =
-                "import time\n" +
-                "for x in xrange(0, 5):\n" +
-                "    print 'TestPrint'\n" +
-                "    time.sleep(1)\n";
-
-            // The issue here is that the whole ExecuteScript will run
-            // Until it is finished. I would like to be able to exit it
-            // so that Unity can do its rendering and then go back to
-            // continuing execution once one frame has been rendered.
-            _scriptEngine.ExecuteScript(pythonSource);
-
-            Debug.Log("Finished running script.");
+            _scriptEngine.ExecuteScript(this.script.text);
         }
     }
+    
+    #endregion
 
-    void printMessage(string s)
-    {
-        Debug.Log(s);
-        // Here is where I would like to rerender the scene or be able to go
-        // back to the main loop so that the message gets printed each time.
-        // In my application I am not using Debug.Log, but instead a custom
-        // print function that prints to the console which is a textfield.
-        // The main issue is the same though. All outputs will be printed
-        // once the whole "ExecuteScript" function is finished. I want to
-        // be able to see each individual print once they happen.
-    }
 }
